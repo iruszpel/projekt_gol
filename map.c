174b72fbe4c9de75383b459117486ade67e35e62
JGoliszewski <45105582+jgoliszewski@users.noreply.github.com>
2021-02-28 18:23:27 +0100

    readMap, writeMap, make
    gotowe funkcje czytania i zapisu do pliku oraz makefile.

--- map.c
+++ map.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "map.h"
 
 int **allocateMap(int r, int c)
@@ -21,7 +23,7 @@ int **copyMap(int **oldMap, int r, int c)
     for (int i = 0; i < r; i++)
     {
         mat[i] = calloc(c, sizeof(**mat));
-        memcpy(mat[i],oldMap[i],sizeof(**mat)*c);
+        memcpy(mat[i], oldMap[i], sizeof(**mat) * c);
     }
     return mat;
 }
@@ -38,10 +40,43 @@ void freeMap(int **map, int r)
 
 Map *readMap(char *file)
 {
-    //Do napisania
+    Map *map = malloc(sizeof *map);
+    FILE *in = fopen(file, "r");
+
+    fscanf(in, "%d %d", &map->r, &map->c);
+    map->data = allocateMap(map->r, map->c);
+
+    for (int i = 0; i < map->r; i++)
+    {
+        for (int j = 0; j < map->c; j++)
+        {
+            fscanf(in, "%d", &map->data[i][j]);
+        }
+    }
+
+    return map;
 }
 
-void saveMap(Map *map, char *file)
+void saveMap(Map *map, char *file, int iter)
 {
-    //Do napisania
+    char *folder = malloc(strlen(file) - 4);
+    strncpy(folder, file, strlen(file) - 4);
+    mkdir(folder, 0700);
+
+    int new_len = strlen(file) + 10;
+    char *filename = malloc(new_len);
+
+    snprintf(filename, new_len, "./%s/iter_%d.txt", folder, iter);
+
+    FILE *out = fopen(filename, "w+");
+    fprintf(out, "%d %d\n", map->r, map->c);
+
+    for (int i = 0; i < map->r; i++)
+    {
+        for (int j = 0; j < map->c; j++)
+        {
+            fprintf(out, "%d ", map->data[i][j]);
+        }
+        fprintf(out, "\n");
+    }
 }
--- map.h
+++ map.h
@@ -10,6 +10,6 @@ int **allocateMap(int r, int c); /*Allocates memory for a map*/
 int **copyMap(int **oldMap, int r, int c);
 void freeMap(int **map, int r);
 Map *readMap(char *file); /*Reads map from file and returns Map struct */
-void saveMap(Map *map, char *file); /*Writes map to file */
+void saveMap(Map *map, char *file, int iter); /*Writes map to file */
 
 #endif
